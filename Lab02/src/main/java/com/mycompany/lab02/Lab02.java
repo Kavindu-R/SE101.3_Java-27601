6478b1914ac482f5a9ce85dc1fcc7d9de38f5eb3
Kavindu-R <114651445+Kavindu-R@users.noreply.github.com>
2023-07-30 23:14:51 +0530

    Add files via upload

---
+++ Lab02/src/main/java/com/mycompany/lab02/Lab02.java
@@ -0,0 +1,32 @@
+package com.mycompany.lab02;
+
+public class Lab02 {
+
+    public static void main(String[] args) {
+        char grade = 'A';
+        switch (grade) {
+            case 'A':
+                System.out.println("Excellent!");
+                break;
+            case 'D':
+                System.out.println("You passed");
+            case 'F':
+                System.out.println("Better try again");
+                break;
+            default:
+                System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+
+        if (grade == 'A') {
+            System.out.println("Excellent!");
+        } else if (grade == 'D') {
+            System.out.println("You passed");
+        } else if (grade == 'F') {
+            System.out.println("Better try again");
+        } else {
+            System.out.println("Invalid grade");
+        }
+        System.out.println("Your grade is " + grade);
+    }
+}
